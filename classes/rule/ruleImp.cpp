f9424cbd2cbe347443b9445b2a34431b93e265ae
Joe King <formichi3@gmail.com>
2017-02-24 21:06:26 -0800

    full rule contructor

--- classes/rule/ruleImp.cpp
+++ classes/rule/ruleImp.cpp
@@ -1,5 +1,7 @@
 #include "Rule_H.h"
 
+using namespace std;
+
 rule::rule(){
   name = "";
   logOperator = 0;
@@ -7,5 +9,15 @@ rule::rule(){
 rule::rule(string p_name){
   this->name = p_name;
 }
+rule::rule(string p_name, vector < string > p_args, int p_Operator, vector < vector < string > > p_rules, vector < vector < string > > p_facts ){
+  name = p_name;
+  args = p_args;
+  logOperator = p_Operator;
+  rules = p_rules;
+  facts = p_facts;
+}
 rule::~rule(){
+}
+void rule::print(){
+  cout << name << endl;
 }
