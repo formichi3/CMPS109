ee37e2a7cdf359840bfe3efa361c6076ce6715f8
Joe <formichi3@gmail.com>
2017-03-06 23:01:11 -0800

    added == for fact and deny duplicate fact entry

--- classes/test/ActualActualInfer/RBImp.cpp
+++ classes/test/ActualActualInfer/RBImp.cpp
@@ -14,7 +14,7 @@ RB::~RB(){
 void RB::add(rule p_rule){
   int match = 0;
   pair<string, rule> p_insert (p_rule.name, p_rule);
-  vector<rule> sameKeyRules = this->traversRule(p_rule.name);
+  vector<rule> sameKeyRules = this->traverseRule(p_rule.name);
   if(sameKeyRules.size() != 0){
     for (auto it = sameKeyRules.begin(); it != sameKeyRules.end(); it++){
       if (p_rule == *it){
@@ -48,12 +48,11 @@ string RB::dump(){
 void RB::drop(string p_name){
   this->hash.erase(p_name);
 }
-vector<rule> RB::traversRule(string p_name){
+vector<rule> RB::traverseRule(string p_name){
   auto range = hash.equal_range(p_name);
   vector <rule> matchingRules;
   for (auto x = range.first; x != range.second; x++){
     matchingRules.push_back(x->second);
   }
-  cout << endl;
   return matchingRules;
 }
